c226be0162e73509fa1c63f27f08d32e00c5bb13
Naman Sharma <100354151+NorMAN-ShARMA@users.noreply.github.com>
2022-05-12 18:16:17 +0530

    Add files via upload

---
+++ output of assignment 3.py
@@ -0,0 +1,55 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/Class Material/Python/assignment 3.py ==============
+Q1
+Enter a number10
+Binary equivalent of 10 is 1010
+Q2
+Enter expression:20
+20 = 20
+Q3
+Enter an integer1
+a) 9
+b) 0.0
+enter radius5
+c) 314.1592653589793
+Enter angle 1:60
+Enter angle 1:60
+d) 2.23606797749979
+Enter x coordinate of first point:5
+Enter y coordinate of first point:6
+Enter x coordinate of second point:7
+Enter y coordinate of second point:8
+e) 1.0
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:20
+Enter number of hydrogen atoms in molecule:40
+Enter number of oxygen atoms in molecule:5
+The molecular weight of the compound is 360.52860000000004
