537ae5a024895aa325d6c933ee2de22763d2711f
Naman Sharma <100354151+NorMAN-ShARMA@users.noreply.github.com>
2022-05-27 22:46:35 +0530

    Add files via upload

---
+++ output of assignment 4.py
@@ -0,0 +1,37 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/Class Material/Python/assignment 4.py ==============
+Q1
+Enter mark:60
+Grade: B
+Continue?(y/n)y
+Enter mark:90
+Grade: A
+Continue?(y/n)n
+Q2
+Enter year-2020
+Not a leap year
+Q3
+Question 1:9x1=9
+Right!
+Question 2:9x9=81
+Right!
+Question 3:3x3=8
+Wrong. The correct answer is 9
+Question 4:7x9=63
+Right!
+Question 5:5x0=0
+Right!
+Question 6:0x7=5
+Wrong. The correct answer is 0
+Question 7:7x6=42
+Right!
+Question 8:6x9=8
+Wrong. The correct answer is 54
+Question 9:8x0=8
+Wrong. The correct answer is 0
+Question 10:9x9=81
+Right!
+Q4
+Answer- 177
